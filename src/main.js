ef105e8e14c01a19869896f6753817ea15bcca62
Aleksandr Tochilin <alex8079@gmail.com>
2023-07-02 12:13:49 +0300

    minor fixes

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@
 document.querySelector(".burger-menu").addEventListener("click", () => {
 	document.querySelector(".burger-menu").classList.toggle("burger-menu-close");
 	document.querySelector(".menu").classList.toggle("menu-show");
-	document.querySelector("body").classList.toggle("no-scroll");
+	document.querySelector("body").classList.add("no-scroll");
 });
 document.querySelectorAll(".menu-link").forEach (item => {
 	item.addEventListener("click", () => {
-		document.querySelector("body").classList.toggle("no-scroll");
-		document.querySelector(".burger-menu").classList.toggle("burger-menu-close");
-		document.querySelector(".menu").classList.toggle("menu-show");		
+		document.querySelector("body").classList.remove("no-scroll");
+		document.querySelector(".burger-menu").classList.remove("burger-menu-close");
+		document.querySelector(".menu").classList.remove("menu-show");		
 	});
 })
+
 // slider
 const images = [{
 	url: './images/slider-image1.jpg'
